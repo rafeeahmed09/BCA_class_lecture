ba769de6f506c6bde383280a8556d2e81caf4caa
junaid95082 <alamjunaid1576@gmail.com>
2025-04-19 09:19:41 +0530

    Update Lab_assingment.c

--- C_lecture/19April/Lab_assingment.c
+++ C_lecture/19April/Lab_assingment.c
@@ -28,3 +28,38 @@ int main()
 
     return 0;
 }
+
+// Wap to copy one string into another string with string function & without string function
+
+// With strcpy Function
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[] = "Junaid Alam";
+    char str2[20];
+    strcpy(str2,str);
+    printf("String 1: %s \n" , str);
+    printf("String 2: %s \n" , str2);
+    return 0;
+}
+
+// Without strcpy Function
+#include <stdio.h>
+
+int main()
+{
+    char str[] = "Krish Singhaniya";
+    char str2[20];
+    int i;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        str2[i] = str[i];
+    }
+    str2[i] = '\0';
+    printf("String 1: %s \n", str);
+    printf("String 2: %s \n", str2);
+
+    return 0;
+}
