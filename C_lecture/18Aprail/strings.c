2544139cdc70792ce13ef2cb3b33852356734b9b
Arman00786165 <arman.ahmad.492989@gmail.com>
2025-04-21 11:14:31 +0530

    Update strings.c

--- C_lecture/18Aprail/strings.c
+++ C_lecture/18Aprail/strings.c
@@ -115,3 +115,37 @@
 
      return 0;
 }
+// # Find Length of String
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "Hello, world!";
+    int length = (str);
+    printf("Length of the string is: %d\n", length);
+    return 0;
+}
+// # W.A.P to initialised String Statically
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+
+    int a = 10, b = 20;
+
+    printf("\n a= %d", a);
+    printf("\n b= %d", b);
+    swap(&a, &b);
+    printf("\n a= %d", a);
+    printf("\n b= %d", b);
+
+    int c;
+    c = a;
+    a = b;
+    c = a;
+
+    printf("%d %d", a, b);
+    return 0;
+}
