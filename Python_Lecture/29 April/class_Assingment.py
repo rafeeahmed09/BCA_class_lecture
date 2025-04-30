6cd8cc9469042df33f5213902962ee2ff64bc4b1
PDX PRO <pdxpro0098@gmail.com>
2025-04-30 05:47:34 +0530

    add 29 april class_Assignment

---
+++ Python_Lecture/29 April/class_Assingment.py
@@ -0,0 +1,19 @@
+# # WAP to find is list empty or not
+lst = []
+print("list is not empty" if lst else "list is empty")
+
+# lst = [1, 2, 3, 4, 5]
+print("list is not empty" if lst else "list is empty")
+
+# # WAP to find largest and smallest by using function and find diff btw these two
+lst = [12, 45, 67, 89, 90]
+print(max(lst))
+print(min(lst))
+print("Different between numbers :", max(lst)-min(lst))
+
+
+# WAP to find largest sting in list
+# ["ABC","ABCDE","ABCD","AB"]
+
+lst = ["ABC","ABCDE","ABCD","AB"]
+print(max(lst))
