363dd50c243bf4cbf244120264712552246ebea9
junaid95082 <alamjunaid1576@gmail.com>
2025-04-30 11:21:22 +0530

    Create Enums_Union.c

---
+++ C_lecture/29 April/Enums_Union.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+union name
+{
+    int a;
+    int b;
+    int c;
+};
+
+enum days
+{
+    sun,
+    mon,
+    tue,
+    wed,
+    thu,
+    fri,
+    sat,
+};
+
+int main()
+{
+    union name u1;
+    u1.a = 7;
+    // printf("%d", u1.a);
+
+    u1.c = 5;
+    // printf("%d", u1.b);
+
+    u1.c = 2;
+    // printf("%d", u1.c);
+
+    enum days wd = mon;
+    printf("%d",wd);
+
+    return 0;
+}
