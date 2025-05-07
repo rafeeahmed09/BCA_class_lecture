4d828a2bcd99fd9167f338232ae6a4aba9f69129
pdxpro0098 <125691886+pdxpro0098@users.noreply.github.com>
2025-05-07 20:57:38 +0530

    Create main.c

---
+++ C_lecture/6May/main.c
@@ -0,0 +1,45 @@
+ #include <stdio.h>
+
+    int main()
+    {
+    for (int i = 0; i <= 100; i++)
+    {
+    if (i % 2 != 0)
+    {
+    continue;
+    }
+    printf("%d ", i);
+    }
+    return 0;
+    }
+    #include <stdio.h>
+
+    int main()
+    {
+    for (int i = 0; i <= 10; i++)
+    {
+    if (i == 5)
+    {
+    break;
+    }
+    printf("%d ", i);
+    }
+    return 0;
+    }
+
+    #include<stdio.h>
+
+int main()
+{
+    int i = 0;
+again:
+    printf("%d", i);
+    i++;
+    if (i == 11)
+        goto end;
+
+    goto again;
+end:;
+
+    return 0;
+}
