4a2a70771e126f047ad361c2e0eed3f2fb14e249
PDX PRO <pdxpro0098@gmail.com>
2025-05-17 23:24:23 +0530

    add Q3

--- C_lecture/17May/class_assingment.py
+++ C_lecture/17May/class_assingment.py
@@ -17,21 +17,33 @@
 
 # WAP to check is a number is armstrong number or not
 
-import math
+# import math
 
-num = 153
-numCP = num
-asm = 0
+# num = 153
+# numCP = num
+# asm = 0
 
-length = len(str(num))
+# length = len(str(num))
 
-for i in range(length):
-    ldPow = math.pow(numCP % 10, length)
-    numCP //= 10
-    asm += ldPow
+# for i in range(length):
+#     ldPow = math.pow(numCP % 10, length)
+#     numCP //= 10
+#     asm += ldPow
 
-if num == int(asm):
-    print(int(asm), " Is armstrong")
-else:
-    pass
-    print(int(asm), " Is not armstrong")
+# if num == int(asm):
+#     print(int(asm), " Is armstrong")
+# else:
+#     pass
+#     print(int(asm), " Is not armstrong")
+
+# arr = [[None, None, None],
+#        [None, None, None],
+#        [None, None, None]]
+# num = 1
+
+# for i in range(3):
+#     for j in range(3):
+#         arr[i][j] = num
+#         num += 1
+
+# print(arr)
