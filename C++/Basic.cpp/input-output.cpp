3e16f0db92ae02315a7c2b0f42a08e1cf8d76e35
pdxpro0098 <125691886+pdxpro0098@users.noreply.github.com>
2025-08-20 15:34:50 +0530

    Create input-output.cpp

---
+++ C++/Basic.cpp/input-output.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // output | printing to terminal
+    // syntax
+    // cout << variable | value ;
+
+    cout << "Krish";         // output | print without next line
+    cout << "Krish" << endl; // output | print with next line
+    cout << "Krish" << "\n"; // output | print with next line (preferred)
+
+    // input from terminal
+    // syntax
+    // cin >> variable;
+    int a;
+    cin >> a;          // taking input
+    cout << a << endl; // output | print with next line
+
+    return 0;
+}
